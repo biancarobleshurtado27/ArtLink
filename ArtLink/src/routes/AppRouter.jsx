e97b4d7f2a867f65dadd3245564a434e79436656
BiancaRobles27 <biancarobleshurtado27@gmail.com>
2026-09-25 11:10:13 -0600

    fix: corregir componente indefinido en AppRouter

--- ArtLink/src/routes/AppRouter.jsx
+++ ArtLink/src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from '../layouts/AppLayout'
 import HomePage from '../pages/HomePage'
 import ExplorePage from '../pages/ExplorePage'
